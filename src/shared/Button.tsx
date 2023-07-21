e4d4ca50ef1453ff072241110e4e6d3d0aa17e7d
曹珂俭 <1849201815@qq.com>
2023-07-21 21:42:36 +0800

    增加Button功能

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,18 +1,44 @@
-import { PropType, defineComponent, ref } from 'vue';
+import { PropType, computed, defineComponent, ref } from 'vue';
 import s from './Button.module.scss';
 export const Button = defineComponent({
   props: {
     onClick: {
       type: Function as PropType<(e: MouseEvent) => void>
     },
+    level: {
+      type: String as PropType<'important' | 'danger' | 'normal'>,
+      default: 'important'
+    },
+    type: {
+      type: String as PropType<'submit' | 'button'>,
+      default: 'button'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    autoSelfDisabled: {
+      type: Boolean,
+      default: false
+    }
   },
   setup: (props, context) => {
     const selfDisabled = ref(false)
+    const _disabled = computed(() => {
+      if(props.autoSelfDisabled === false){
+        return props.disabled
+      }
+      if (selfDisabled.value) {
+        return true
+      } else {
+        return props.disabled
+      }
+    })
     const onClick = (e: MouseEvent) => {
       if (!selfDisabled.value) {
         props.onClick?.(e)
         selfDisabled.value = true
-      }else{
+      } else {
         return
       }
       console.log(selfDisabled.value);
@@ -21,7 +47,7 @@ export const Button = defineComponent({
       }, 1000)
     }
     return () => (
-      <button onClick={onClick} class={s.button}>{context.slots.default?.()}</button>
+      <button onClick={onClick}disabled={_disabled.value} type={props.type} class={[s.button, s[props.level]]}>{context.slots.default?.()}</button>
     )
   }
 })
