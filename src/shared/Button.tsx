1b384d469fd88bd7d2fc907de5900261308972dd
曹珂俭 <1849201815@qq.com>
2023-07-21 20:22:28 +0800

    button添加节流

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -2,13 +2,26 @@ import { PropType, defineComponent, ref } from 'vue';
 import s from './Button.module.scss';
 export const Button = defineComponent({
   props: {
-    name: {
-      type: String as PropType<string>
-    }
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>
+    },
   },
   setup: (props, context) => {
-    return () => ( 
-      <button class={s.button}>{context.slots.default?.()}</button>
+    const selfDisabled = ref(false)
+    const onClick = (e: MouseEvent) => {
+      if (!selfDisabled.value) {
+        props.onClick?.(e)
+        selfDisabled.value = true
+      }else{
+        return
+      }
+      console.log(selfDisabled.value);
+      setTimeout(() => {
+        selfDisabled.value = false
+      }, 1000)
+    }
+    return () => (
+      <button onClick={onClick} class={s.button}>{context.slots.default?.()}</button>
     )
   }
 })
