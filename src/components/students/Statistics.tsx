8d1c071dc9bbdeb3af12e453818bf1ec63e0e766
曹珂俭 <1849201815@qq.com>
2023-08-06 21:09:58 +0800

    feat(): 准备打造选项卡

--- src/components/students/Statistics.tsx
+++ src/components/students/Statistics.tsx
@@ -1,5 +1,8 @@
-import { PropType, defineComponent, ref } from 'vue';
+import { PropType, defineComponent, reactive, ref } from 'vue';
 import s from './Statistics.module.scss';
+import { MainLayout } from '../../layouts/MainLayout';
+import { BackIcon } from '../../shared/BackIcon';
+import { MenuBar } from '../../layouts/MenuBar';
 export const Statistics = defineComponent({
   props: {
     name: {
@@ -7,8 +10,24 @@ export const Statistics = defineComponent({
     }
   },
   setup: (props, context) => {
+    const isBoolean = reactive({
+      isShowMenu: false
+    })
     return () => (
-      <div>Statistics</div>
+      <MainLayout>{
+        {
+          icon: () => <BackIcon svg='menu' onClick={() => isBoolean.isShowMenu = true}/>,
+          title: () => '作业统计',
+          default:() => <div class={s.wrapper}>
+            123
+            {
+              isBoolean.isShowMenu ?
+              <MenuBar onClose={() => isBoolean.isShowMenu = false} />
+              : null
+            }
+          </div>
+        }
+      }</MainLayout>
     )
   }
 })
