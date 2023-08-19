ac6a5dfa21fda7e739bbd9c76364e531e2b78f4a
曹珂俭 <1849201815@qq.com>
2023-08-19 22:27:05 +0800

    feat: 封装权限页

---
+++ src/components/error/Authority.tsx
@@ -0,0 +1,28 @@
+import { PropType, defineComponent, ref } from 'vue';
+import s from './Authority.module.scss';
+import { MainLayout } from '../../layouts/MainLayout';
+import { BackIcon } from '../../shared/BackIcon';
+import { Button } from '../../shared/Button';
+import { useRouter } from 'vue-router';
+import { removeLocal } from '../../config/utils';
+export const Authority = defineComponent({
+  setup: (props, context) => {
+    const router = useRouter()
+    const goToLogin = () => {
+      router.push('/login')
+      removeLocal()
+    }
+    return () => (
+      <MainLayout>{
+        {
+          icon: () => <BackIcon svg='goLogin'/>,
+          title: () => '权限错误',
+          default: () => <div class={s.wrapper}>
+              <svg class={s.svg}><use xlinkHref='#noAuth'></use></svg>
+              <Button onClick={() => goToLogin()}>去登录</Button>
+          </div>
+        }
+      }</MainLayout>
+    )
+  }
+})
