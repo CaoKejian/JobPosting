52d858b7f95718cd5c8145512604d0615672382b
曹珂俭 <1849201815@qq.com>
2023-09-13 20:11:31 +0800

    feat: 完成老师数据分析页面架构

--- src/components/teacher/Analyze.tsx
+++ src/components/teacher/Analyze.tsx
@@ -1,14 +1,12 @@
 import { PropType, defineComponent, ref } from 'vue';
 import s from './Analyze.module.scss';
+import { TabsLayout } from '../../layouts/TabsLayout';
+import { AnalyzeList } from '../teacher/AnalyzeList';
 export const Analyze = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
+    const tabMap = ['监控', '难度评估', '分析提交', '模型预测']
     return () => (
-      <div>敬请期待</div>
+      <TabsLayout component={AnalyzeList} tabMap={tabMap} title='数据分析' />
     )
   }
 })
---
+++ src/components/teacher/AnalyzeList.tsx
@@ -0,0 +1,25 @@
+import { PropType, defineComponent, ref } from 'vue';
+import s from '../students/Analyze.module.scss';
+export const AnalyzeList = defineComponent({
+  props: {
+    id: {
+      type: String as PropType<string>
+    }
+  },
+  setup: (props, context) => {
+    return () => (<div class={s.wrapper} style={{ fontSize: '1rem' }}>
+      {
+        props.id === '0' ? (
+          1
+        ) : props.id === '1' ? (
+          2
+        ) : props.id === '2' ? (
+          3
+        ) : props.id === '3' ? (
+          4
+        ) : <div>网页异常，请刷新重试！</div>
+      }
+    </div>
+    )
+  }
+})
