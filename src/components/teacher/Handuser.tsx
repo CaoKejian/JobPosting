e0f097c240bfbf9ef05dffb9ac0f3c6afb849ebc
曹珂俭 <1849201815@qq.com>
2023-09-20 22:02:46 +0800

    feat: 完成上传用户页面布局

--- src/components/teacher/Handuser.tsx
+++ src/components/teacher/Handuser.tsx
@@ -1,14 +1,40 @@
-import { PropType, defineComponent, ref } from 'vue';
+import { PropType, defineComponent, reactive, ref, toRefs } from 'vue';
 import s from './Handuser.module.scss';
+import { MainLayout } from '../../layouts/MainLayout';
+import { BackIcon } from '../../shared/BackIcon';
+import { Quote } from '../../shared/Quote';
+import { MenuBar } from '../../layouts/MenuBar';
+import { Button } from '../../shared/Button';
 export const Handuser = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
+    const useData = reactive({
+      isShowMenu: false,
+      status: '正在上传...'
+    })
+    const { isShowMenu, status } = toRefs(useData)
     return () => (
-      <div>Handuser</div>
+      <MainLayout>{
+        {
+          icon: () => <BackIcon svg='menu' onClick={() => isShowMenu.value = true} />,
+          title: () => '上传用户',
+          default: () => <div class={s.wrapper}>
+            <Quote name='上传csv文件，需至少姓名、学号两列数据' />
+            <div class={s.upload}>
+              <van-uploader>
+                <Button>上传表单文件</Button>
+              </van-uploader>
+            </div>
+            <div class={s.status}><svg class={s.svg}><use xlinkHref='#isHandinfo'></use></svg>{status.value}</div>
+            <p>注：此过程会持续导入表单学生信息</p>
+            <p>保证表格有<span class={s.content}>name</span>（姓名）和<span class={s.content}>stuId</span>（学号）两列数据</p>
+            {
+              isShowMenu.value ?
+                <MenuBar onClose={() => isShowMenu.value = false} />
+                : null
+            }
+          </div>
+        }
+      }</MainLayout>
     )
   }
 })
