5f0ded37e0602b6713c157f115d02fef91fce50e
曹珂俭 <1849201815@qq.com>
2023-09-20 22:43:11 +0800

    feat: 完成上传

--- src/components/teacher/Handuser.tsx
+++ src/components/teacher/Handuser.tsx
@@ -5,13 +5,53 @@ import { BackIcon } from '../../shared/BackIcon';
 import { Quote } from '../../shared/Quote';
 import { MenuBar } from '../../layouts/MenuBar';
 import { Button } from '../../shared/Button';
+import { Toast } from 'vant';
+import { http } from '../../shared/Http';
 export const Handuser = defineComponent({
   setup: (props, context) => {
     const useData = reactive({
       isShowMenu: false,
-      status: '正在上传...'
+      formData: [],
+      status: '',
+      svg: '#isHandinfo',
+      action: 'running'
     })
-    const { isShowMenu, status } = toRefs(useData)
+    const { isShowMenu, formData, status, svg, action } = toRefs(useData)
+    const afterRead = (file: any) => {
+      let formDataFile = new FormData();
+      formDataFile.append('file', file.file);
+      console.log(file)
+      console.log(formDataFile.get('file'))
+      http.post('/upload/file', formDataFile, {
+        _autoLoading: true
+      }).then((response: any) => {
+        console.log(response)
+        Toast({
+          message: `上传成功！`,
+        });
+        Object.assign(formData, {
+          file: {
+            fileName: response.data.fileName,
+            fileUrl: response.data.url
+          }
+        });
+        status.value = '数据正在处理入库，请稍后...'
+        setTimeout(() => {
+          handleData()
+        }, 2000);
+      }).catch(error => {
+        Toast({
+          message: `上传失败！${error}`,
+        });
+        status.value = '处理失败，请检查网络原因！'
+        svg.value = '#error'
+      });
+    }
+    const handleData = () => {
+      status.value = '处理完成！'
+      svg.value = '#success'
+      action.value = 'paused'
+    }
     return () => (
       <MainLayout>{
         {
@@ -20,11 +60,23 @@ export const Handuser = defineComponent({
           default: () => <div class={s.wrapper}>
             <Quote name='上传csv文件，需至少姓名、学号两列数据' />
             <div class={s.upload}>
-              <van-uploader>
+              <van-uploader
+                after-read={afterRead}
+                v-model={formData.value}
+                max-count={1}
+                deletable={true}
+                accept=".xlsx"
+              >
                 <Button>上传表单文件</Button>
               </van-uploader>
             </div>
-            <div class={s.status}><svg class={s.svg}><use xlinkHref='#isHandinfo'></use></svg>{status.value}</div>
+            {
+              status.value ?
+                <div class={s.status}><svg class={s.svg} style={{
+                  animationPlayState: `${action.value}`
+                }}><use xlinkHref={svg.value}></use></svg>{status.value}</div>
+                : null
+            }
             <p>注：此过程会持续导入表单学生信息</p>
             <p>保证表格有<span class={s.content}>name</span>（姓名）和<span class={s.content}>stuId</span>（学号）两列数据</p>
             {
