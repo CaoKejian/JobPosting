4eecde142ed9c6eecfe84ccdf0f176040c279051
曹珂俭 <1849201815@qq.com>
2023-09-22 20:54:15 +0800

    feat: 完成难度与预估数据交互

--- src/components/analyze/Difficulty.tsx
+++ src/components/analyze/Difficulty.tsx
@@ -1,18 +1,34 @@
-import { PropType, defineComponent, ref } from 'vue';
+import { PropType, defineComponent, onMounted, ref } from 'vue';
 import s from './WeekFrequency.module.scss';
 import { Quote } from '../../shared/Quote';
 import { DifficultyQuency } from '../charts/teaAnalyze/DifficultyQuenvy';
+import { http } from '../../shared/Http';
+import { Toast } from 'vant';
 export const Difficulty = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
+    const average = ref<number[]>([])
+    const subject = ref<string[]>([])
+    const x= ref<{}[]>([])
+    const fetchData = async (classId:string) => {
+      try{
+        const res = await http.get<{average_score:number, subject:string}[]>('/analyze/difficulty',{classId})
+        const data = res.data
+        x.value=data
+        data.map(item => {
+          average.value.push(item.average_score)
+          subject.value.push(item.subject)
+        })
+      }catch(err){
+        Toast({ message: '网络异常，此为Mock环境！' })
+      }
+    }
+    onMounted(() => {
+      fetchData('123123')
+    })
     return () => (
       <div class={s.wrapper}>
         <Quote name='学科平均分与难度估测'/>
-        <DifficultyQuency />
+        <DifficultyQuency x={x.value} average={average.value} subject={subject.value}/>
       </div>
     )
   }
