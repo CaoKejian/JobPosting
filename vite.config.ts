9e9929d3925f2b98d05815460e3d8a22f2cf1c05
曹珂俭 <1849201815@qq.com>
2023-09-24 23:34:22 +0800

    feat: Rollup Chunk优化！

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import postcsspxtoviewport from "postcss-px-to-viewport" //插件
 import { svgstore } from './src/vite_plugins/svgstore'
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: any) {
+          if (id.includes('echarts')) {
+            return 'echarts';
+          }
+          if (id.includes('vant')) {
+            return 'vant';
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   plugins: [vue(), svgstore(), vueJsx({
     transformOn: true,
     mergeProps: true
   }),
   ],
-  
+
   server: {
     proxy: {
       '/api': {
