2b615d133c4e61af24f410073f090f37a9b74039
曹珂俭 <1849201815@qq.com>
2023-09-24 23:25:11 +0800

    feat: 动态路由减少白屏时间

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,15 +1,5 @@
 import { RouteRecordRaw } from 'vue-router';
-import Statistics from '../components/students/Statistics';
-import DownLoads from '../components/students/Downloads';
-import { Detail } from '../components/students/Detail';
-import HandWork from '../components/students/HandWork';
-import View from '../components/students/View';
-import { Publish } from '../components/teacher/Publish';
-import { Correct } from '../components/teacher/Correct';
-import { Subject } from '../components/teacher/Subject';
 import { Authority } from '../components/error/Authority';
-import { Auth } from '../components/teacher/Auth';
-import { Handuser } from '../components/teacher/Handuser';
 
 export const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/welcome' },
@@ -36,10 +26,10 @@ export const routes: RouteRecordRaw[] = [
     path: '/student', component: () => import('../views/Student'),
     children: [
       { path: '', redirect: '/student/detail' },
-      { path: 'detail', component: Detail },
-      { path: ':id/handWork', component: HandWork },
-      { path: 'statistics', component: Statistics },
-      { path: 'downloads', component: DownLoads },
+      { path: 'detail', component: import('../components/students/Detail') },
+      { path: ':id/handWork', component: import('../components/students/HandWork') },
+      { path: 'statistics', component: import('../components/students/Statistics') },
+      { path: 'downloads', component: import('../components/students/Downloads') },
       { path: 'analyze', component: () => import('../components/students/Analyze') },
     ]
   },
@@ -47,11 +37,11 @@ export const routes: RouteRecordRaw[] = [
     path: '/teacher', component: () => import('../views/Teacher'),
     children: [
       { path: '', redirect: '/teacher/publish' },
-      { path: 'publish', component: Publish },
-      { path: 'correct', component: Correct },
-      { path: 'subject', component: Subject },
-      { path: 'auth', component: Auth },
-      { path: 'handuser', component: Handuser },
+      { path: 'publish', component: import('../components/teacher/Publish') },
+      { path: 'correct', component: import('../components/teacher/Correct') },
+      { path: 'subject', component: import('../components/teacher/Subject') },
+      { path: 'auth', component: import('../components/teacher/Auth') },
+      { path: 'handuser', component: import('../components/teacher/Handuser') },
       { path: 'analyze', component: () => import('../components/teacher/Analyze') },
     ]
   }
