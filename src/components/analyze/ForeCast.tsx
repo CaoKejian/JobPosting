c630ff9f164726ed20c79c5bc8c03761de565261
曹珂俭 <1849201815@qq.com>
2023-09-25 17:38:37 +0800

    feat: 完成预测作业数据交互

--- src/components/analyze/ForeCast.tsx
+++ src/components/analyze/ForeCast.tsx
@@ -1,8 +1,21 @@
-import { PropType, defineComponent, reactive, ref, toRefs } from 'vue';
+import { PropType, defineComponent, onMounted, reactive, ref, toRefs, watch } from 'vue';
 import s from './WeekFrequency.module.scss';
 import { Quote } from '../../shared/Quote';
 import { Button } from '../../shared/Button';
 import { FormItem } from '../../shared/Form';
+import { http } from '../../shared/Http';
+import { useInfoStore } from '../../store/useInfoStore';
+import { Toast } from 'vant';
+interface UseDataType {
+  disable: boolean,
+  selectData: { value: string, text: string }[],
+  selectValue: string,
+  isShowStudentInfo: boolean,
+  studentInfo: {
+    bit: number,
+    score: number
+  }
+}
 export const ForeeCast = defineComponent({
   props: {
     name: {
@@ -10,35 +23,64 @@ export const ForeeCast = defineComponent({
     }
   },
   setup: (props, context) => {
-    const useData = reactive({
+    const infoStore = useInfoStore()
+    const useData = reactive<UseDataType>({
       disable: false,
-      selectData: [
-        { value: 'x', text: 'x' },
-        { value: 'y', text: 'y' },
-      ],
+      selectData: [],
+      selectValue: '',
       isShowStudentInfo: false,
       studentInfo: {
         bit: 0,
         score: 0
       }
     })
-    const { disable, selectData, isShowStudentInfo, studentInfo } = toRefs(useData)
-    const handleFore = () => {
+    const { disable, selectData, selectValue, isShowStudentInfo, studentInfo } = toRefs(useData)
+    const handleForeData = async (stuId: string) => {
+      const res = await http.get<{ bit: number, score: number }>('/analyze/complete', { stuId })
+      const data = res.data
+      Object.assign(studentInfo.value, {
+        bit: (data.bit * 100).toFixed(2) + '%',
+        score: (data.score).toFixed(2)
+      })
+    }
+    const handleFore = async () => {
       disable.value = true
+      const res = await infoStore.nameMapFunction(selectValue.value)
+      handleForeData(res)
+      loading('正在预测，请稍后...')
+    }
+    watch(() => selectValue.value, (n) => {
+      selectValue.value = n
+    })
+    const handleStudentInfo = async () => {
+      const info = infoStore.student
+      for (const item of Object.entries(info)) {
+        const obj = { value: '', text: '' }
+        obj.value = item[0]
+        obj.text = item[1]
+        selectData.value.push(obj)
+      }
+      selectValue.value = selectData.value[0].text
+    }
+    const loading = (x: string) => {
+      Toast.loading({
+        message: x,
+        forbidClick: true,
+      })
       setTimeout(() => {
-        Object.assign(studentInfo.value, {
-          bit: 0.65,
-          score: 70.2
-        })
+        Toast.clear()
         isShowStudentInfo.value = true
         disable.value = false
       }, 1000);
     }
+    onMounted(() => {
+      handleStudentInfo()
+    })
     return () => (
       <div class={s.wrapper}>
         <Quote name='预测提交作业比率和预测得分' />
         <FormItem type='select' label='选择学生'
-          modelValue={selectData.value[0].text}
+          v-model={selectValue.value}
           options={selectData.value}
         >
         </FormItem>
